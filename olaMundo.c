699a4d3b02aa4328cb47b972ce77860abeb29dd5
hpablo-campos <hpablo.campos01@gmail.com>
2025-05-15 18:30:47 +0000

    commit

--- olaMundo.c
+++ olaMundo.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+    // Exibe a mensagem "Olá, Mundo!" na tela
+    printf("Olá, Mundo!\n");
+    
+    // Retorna 0 para indicar que o programa terminou com sucesso
+    return 0;
+}
