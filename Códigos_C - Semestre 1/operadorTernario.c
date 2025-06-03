350c1a6adba705f65eb410c6de24a2572f1b33a2
hpablo-campos <hpablo.campos01@gmail.com>
2025-06-03 16:09:11 +0000

    commit

---
+++ Códigos_C - Semestre 1/operadorTernario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+    int idade = 20;
+    int resultado;
+
+    idade >= 18 ? printf("Maior de idade\n") : printf("Menor de idade\n");
+
+    
+
+
+
+
+
+    return 0;
+}
