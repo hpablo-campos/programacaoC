146fe25905275c999f7ba5e9bd2ff56c17fe7dde
hpablo-campos <hpablo.campos01@gmail.com>
2025-06-05 19:10:48 +0000

    commit

--- Códigos_C - Semestre 1/Modulo 3/xadrez.c
+++ Códigos_C - Semestre 1/Modulo 3/xadrez.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 
+// Método para movimentação da Torre
+
 void moverTorre (int casas){
-    if (casa > 0){
-        printf ("Direita\n");
+    if (casas > 0){
+        printf ("Direita\n\n");
         moverTorre (casas - 1);
     }
 }
 
+void moverBispo (int casas){
+    for (int vertical = casas; casas > 0; casas--){
+        printf ("Cima\n");
+        for (int horizontal = vertical; vertical > 0; vertical --){
+            printf ("Direita\n\n");
+            break;
+        }
+    }
+}
+
+void moverRainha (int casas){
+    if (casas > 0){
+        printf ("Esquerda\n\n");
+        moverRainha (casas -1);
+    }
+}
+
+void moverCavalo (int casas = 2){
+    for (casas > 0; casas--){
+        printf ("Cima\n");
+        for (casas = 1){
+            printf ("Direita\n\n");
+        }
+    }
+
+}
 
 int main (){
 
-    moverTorre(5); // torre anda 5 casas para direita.
+    printf ("** MOVIMENTAÇÃO DA TORRE **\n\n");
+    moverTorre(5); // torre anda 5 casas.
+
+    printf ("\n** MOVIMENTAÇÃO DO BISPO **\n\n");
+    moverBispo (5); // bispo anda 5 casas.
+
+    printf ("\n** MOVIMENTAÇÃO DA RAINHA **\n\n");
+    moverRainha (8); // rainha anda 8 casas.
+
+    moverCavalo ();
     return 0;
 
 }
