42ab017289524e692f90d0db0b8f404c8bcafd09
hpablo-campos <hpablo.campos01@gmail.com>
2025-06-16 17:12:42 +0000

    commit

--- Códigos_C - Semestre 1/Modulo 4/batalhaNaval.c
+++ Códigos_C - Semestre 1/Modulo 4/batalhaNaval.c
@@ -6,22 +6,33 @@ int main() {
 
     int tamanho_navio = 3;
 
-    int navioH_linha = 2;
-    int navioH_coluna = 2;
-
-    int navioV_linha = 5;
-    int navioV_coluna = 6;
-
-    
+    // Navio 1 (Horizontal)
+    int navioH_linha = 0;
+    int navioH_coluna = 0;
     for (int i = 0; i < tamanho_navio; i++) {
         tabuleiro[navioH_linha][navioH_coluna + i] = 3;
     }
 
-    
+    // Navio 2 (Vertical)
+    int navioV_linha = 2;
+    int navioV_coluna = 5;
     for (int i = 0; i < tamanho_navio; i++) {
         tabuleiro[navioV_linha + i][navioV_coluna] = 3;
     }
 
+    // Navio 3 (Diagonal \)
+    int navioD1_linha = 6;
+    int navioD1_coluna = 0;
+    for (int i = 0; i < tamanho_navio; i++) {
+        tabuleiro[navioD1_linha + i][navioD1_coluna + i] = 3;
+    }
+
+    // Navio 4 (Diagonal /)
+    int navioD2_linha = 2;
+    int navioD2_coluna = 9;
+    for (int i = 0; i < tamanho_navio; i++) {
+        tabuleiro[navioD2_linha + i][navioD2_coluna - i] = 3;
+    }
     
     printf("\n=== TABULEIRO DE BATALHA NAVAL ===\n\n");
 
